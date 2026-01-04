9ee6a513b6578d077ebe451433fa1c110582ac23
kranthi-kumar-2004 <159746543+kranthi-kumar-2004@users.noreply.github.com>
2026-01-04 12:56:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const reveals = document.querySelectorAll(".reveal");
 
-const revealOnScroll = () => {
+function revealOnScroll() {
   reveals.forEach(el => {
     const top = el.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-    if (top < windowHeight - 100) {
+    if (top < window.innerHeight - 100) {
       el.classList.add("active");
     }
   });
-};
+}
 
 window.addEventListener("scroll", revealOnScroll);
 window.addEventListener("load", revealOnScroll);

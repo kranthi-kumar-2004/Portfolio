f4b2fe64f3010ea56ac6e63a31f57065ca452b58
kranthi-kumar-2004 <159746543+kranthi-kumar-2004@users.noreply.github.com>
2026-01-04 10:49:29 +0530

    Add lightbox open and close functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function openLightbox(img) {
+  document.getElementById("lightbox").style.display = "flex";
+  document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}function openLightbox(img) {
+  document.getElementById("lightbox").style.display = "flex";
+  document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
